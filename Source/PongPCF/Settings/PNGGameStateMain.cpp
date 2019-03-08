ac4ba0e3c7e441ed05085b14f3bd887fb776c16b
Mykhaylo Hnatyuk <mhnatyuk@n-ix.com>
2019-03-08 15:31:47 +0200

    game mode and visual changes

--- Source/PongPCF/Settings/PNGGameStateMain.cpp
+++ Source/PongPCF/Settings/PNGGameStateMain.cpp
@@ -2,3 +2,150 @@
 
 #include "PNGGameStateMain.h"
 
+APNGGameStateMain::APNGGameStateMain(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bTickEvenWhenPaused = true;
+}
+
+void APNGGameStateMain::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Initialization();
+}
+
+void APNGGameStateMain::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	TArray<FPNGBaseGameState*> GraphToClean;
+	mHandlers.GenerateValueArray(GraphToClean);
+	for (FPNGBaseGameState* GraphState : GraphToClean)
+	{
+		delete GraphState;
+	}
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void APNGGameStateMain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!HasActorBegunPlay())
+	{
+		return;
+	}
+
+	ProcessStateMachine();
+}
+
+void APNGGameStateMain::Initialization()
+{
+	mHandlers.Add(GameStates::gsNoState, new FPNGGSNoSate());
+	mHandlers.Add(GameStates::gsWaitingForPlayers, new FPNGGSWaitingForPlayers());
+	mHandlers.Add(GameStates::gsStartingPlay, new FPNGGSStartingPlay());
+	mHandlers.Add(GameStates::gsPlaying, new FPNGGSPlaying());
+	mHandlers.Add(GameStates::gsExiting, new FPNGGSExiting());
+
+	mTransitions.Add(GameStates::gsNoState, { GameStates::gsWaitingForPlayers });
+	mTransitions.Add(GameStates::gsWaitingForPlayers, { GameStates::gsStartingPlay });
+	mTransitions.Add(GameStates::gsStartingPlay, { GameStates::gsPlaying, GameStates::gsExiting });
+	mTransitions.Add(GameStates::gsPlaying, { GameStates::gsStartingPlay, GameStates::gsExiting });
+	mTransitions.Add(GameStates::gsExiting, { GameStates::gsNoState });
+
+	mCurrentState = GameStates::gsNoState;
+	mDesireState = GameStates::gsWaitingForPlayers;
+
+	FPNGBaseGameState* firstState = mHandlers.FindRef(mCurrentState);
+	firstState->StartState(GetWorld());
+}
+
+void APNGGameStateMain::ProcessStateMachine()
+{
+	GameStates currentStateType = GetState();
+	FPNGBaseGameState* currentState = mHandlers.FindRef(currentStateType);
+
+	currentState->ProcessState(GetWorld());
+
+	if(currentState->IsReadyForExit())
+	{
+		TArray<GameStates> transitions = mTransitions.FindRef(currentStateType);
+		for(auto transitionToSate : transitions)
+		{
+			FPNGBaseGameState* nextState = mHandlers.FindRef(transitionToSate);
+
+			if(nextState->IsReadyForActivation(GetWorld(), this))
+			{
+				UE_LOG(LogType, Log, TEXT("APNGGameStateMain::ProcessStateMachine FROM %d TO %d"), (uint8)mCurrentState, (uint8)transitionToSate);
+				currentState->EndState(GetWorld());
+				mCurrentState = transitionToSate;
+				nextState->StartState(GetWorld());
+
+				if(mDesireState == transitionToSate)
+				{
+					mDesireState = GameStates::gsNoState;
+				}
+
+				OnGameStateChanged.Broadcast(mCurrentState);
+				break;
+			}
+		}
+	}
+}
+
+void APNGGameStateMain::TrySwitchState(GameStates value)
+{
+	mDesireState = value;
+}
+
+void FPNGGSNoSate::StartState(UWorld* World)
+{
+	MarkExit(true);
+}
+
+void FPNGGSWaitingForPlayers::ProcessState(UWorld* World)
+{
+	// Check how many players connected.
+	// if 2 - go to start playing. 
+	MarkExit(true);
+}
+
+bool FPNGGSStartingPlay::IsReadyForActivation(UWorld* World, APNGGameStateMain* GameStateActor) const
+{
+	return GameStateActor->GetState() == GameStates::gsWaitingForPlayers || GameStateActor->GetDesireState() == GameStates::gsStartingPlay;
+}
+
+void FPNGGSStartingPlay::StartState(UWorld* World)
+{
+	FPNGBaseGameState::StartState(World);
+
+	mStartTimer = 0.0f;
+}
+
+#define DELAY_BEFORE_START 1.0f
+void FPNGGSStartingPlay::ProcessState(UWorld* World)
+{
+	mStartTimer += World->GetDeltaSeconds();
+
+	if(mStartTimer > DELAY_BEFORE_START)
+	{
+		MarkExit(true);
+	}
+}
+
+void FPNGGSPlaying::StartState(UWorld* World)
+{
+	// We are ready to exit whenever any other state is needed.
+	MarkExit(true);
+}
+
+void FPNGGSPlaying::ProcessState(UWorld* World)
+{
+
+}
+
+bool FPNGGSExiting::IsReadyForActivation(UWorld* World, APNGGameStateMain* GameStateActor) const
+{
+	// if we have less than 2 players
+	return false;
+}
--- Source/PongPCF/Settings/PNGGameStateMain.h
+++ Source/PongPCF/Settings/PNGGameStateMain.h
@@ -6,12 +6,137 @@
 #include "GameFramework/GameState.h"
 #include "PNGGameStateMain.generated.h"
 
-/**
- * 
- */
+
+struct FPNGBaseGameState;
+
+UENUM(BlueprintType)
+enum class GameStates : uint8
+{
+	gsNoState				UMETA(DisplayName = "NoState"),
+	gsWaitingForPlayers		UMETA(DisplayName = "WaitingForPlayers"),
+	gsStartingPlay			UMETA(DisplayName = "StartingPlay"),
+	gsPlaying				UMETA(DisplayName = "Playing"),
+	gsExiting				UMETA(DisplayName = "Exiting")
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPNGOnGameStateChanged, GameStates, NewState);
+
 UCLASS()
-class PONGPCF_API APNGGameStateMain : public AGameState
+class PONGPCF_API APNGGameStateMain : public AGameStateBase
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	APNGGameStateMain(const FObjectInitializer& ObjectInitializer);
+	virtual ~APNGGameStateMain() = default;
+
+	FPNGOnGameStateChanged OnGameStateChanged;
+
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void Tick(float DeltaTime) override;
+
+	void Initialization();
+	void ProcessStateMachine();
+
+public:
+
+	void TrySwitchState(GameStates value);
+	GameStates GetState() const { return mCurrentState; }
+	GameStates GetDesireState() const { return mDesireState; }
+
+private:
+
+	TMap<GameStates, FPNGBaseGameState*> mHandlers;
+	TMap<GameStates, TArray<GameStates>> mTransitions;
+
+	GameStates mCurrentState;
+	GameStates mDesireState;
+};
+
+USTRUCT()
+struct FPNGBaseGameState
+{
+	GENERATED_USTRUCT_BODY()
+
+	virtual ~FPNGBaseGameState() = default;
+
+	virtual bool IsReadyForActivation(UWorld* World, APNGGameStateMain* GameStateActor) const { return true; }
+	virtual void StartState(UWorld* World) { MarkExit(false); };
+	virtual void ProcessState(UWorld* World) {};
+	virtual void EndState(UWorld* World) {};
+
+	bool IsReadyForExit() const { return mIsReadyForExit; }
+
+protected:
+
+	void MarkExit(bool Value) { mIsReadyForExit = Value; }
+
+private:
+
+	bool mIsReadyForExit;
+};
+
+USTRUCT()
+struct FPNGGSNoSate : public FPNGBaseGameState
+{
+	GENERATED_USTRUCT_BODY()
+
+	virtual	~FPNGGSNoSate() override = default;
+
+	virtual void StartState(UWorld* World) override;
+};
+
+USTRUCT()
+struct FPNGGSWaitingForPlayers : public FPNGBaseGameState
+{
+	GENERATED_USTRUCT_BODY()
+
+	virtual	~FPNGGSWaitingForPlayers() override = default;
+
+	void ProcessState(UWorld* World) override;
+};
+
+USTRUCT()
+struct FPNGGSStartingPlay : public FPNGBaseGameState
+{
+	GENERATED_USTRUCT_BODY()
+
+	virtual	~FPNGGSStartingPlay() override = default;
+
+	bool IsReadyForActivation(UWorld* World, APNGGameStateMain* GameStateActor) const override;
+
+	void StartState(UWorld* World) override;
+
+	void ProcessState(UWorld* World) override;
+
+private:
+
+	// Timer used to make delay before starting play.
+	float mStartTimer;
+};
+
+USTRUCT()
+struct FPNGGSPlaying : public FPNGBaseGameState
+{
+	GENERATED_USTRUCT_BODY()
+
+	virtual	~FPNGGSPlaying() override = default;
+
+	void StartState(UWorld* World) override;
+
+	void ProcessState(UWorld* World) override;
+};
+
+USTRUCT()
+struct FPNGGSExiting : public FPNGBaseGameState
+{
+	GENERATED_USTRUCT_BODY()
+
+	virtual	~FPNGGSExiting() override = default;
+
+	bool IsReadyForActivation(UWorld* World, APNGGameStateMain* GameStateActor) const override;
 };
