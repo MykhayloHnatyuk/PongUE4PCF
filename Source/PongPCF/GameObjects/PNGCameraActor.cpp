45a800499f2e5dcde1a18ee7cdb8cb9890f00726
Mykhaylo Hnatyuk <mhnatyuk@n-ix.com>
2019-03-10 02:11:05 +0200

    camera color update

--- Source/PongPCF/GameObjects/PNGCameraActor.cpp
+++ Source/PongPCF/GameObjects/PNGCameraActor.cpp
@@ -3,9 +3,18 @@
 #include "PNGCameraActor.h"
 #include "Engine.h"
 
+APNGCameraActor::APNGCameraActor(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+	GetCameraComponent()->PostProcessSettings.bOverride_SceneColorTint = true;
+}
+
 void APNGCameraActor::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	mColorAngle = DefaultColorAngle;
+	mColorUpdateSpeed = DefaultColorUpdateSpeed;
 
 	if (GetWorld())
 	{
@@ -19,3 +28,24 @@ void APNGCameraActor::BeginPlay()
 		}
 	}
 }
+
+void APNGCameraActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UpdateColor(DeltaTime);
+}
+
+void APNGCameraActor::UpdateColor(float DeltaTime)
+{
+	// Lets imagine that we have a color wheel where red is at 0 degree, green at 120 and blue at 240
+	// https://en.wikipedia.org/wiki/File:Simple_RGB_color_wheel.png
+	// We will interpolate through it.
+
+	mColorAngle += mColorUpdateSpeed * DeltaTime;
+	mColorAngle = FMath::Fmod(mColorAngle, 360.0f);
+	
+	const FLinearColor color = ColorCurve->GetClampedLinearColorValue(mColorAngle);
+	GetCameraComponent()->PostProcessSettings.SceneColorTint = FLinearColor(color);
+}
+
--- Source/PongPCF/GameObjects/PNGCameraActor.h
+++ Source/PongPCF/GameObjects/PNGCameraActor.h
@@ -6,9 +6,6 @@
 #include "Camera/CameraActor.h"
 #include "PNGCameraActor.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class PONGPCF_API APNGCameraActor : public ACameraActor
 {
@@ -16,6 +13,28 @@ class PONGPCF_API APNGCameraActor : public ACameraActor
 
 protected:
 
+	APNGCameraActor(const FObjectInitializer& ObjectInitializer);
+
 	virtual void BeginPlay() override;
-	
+	virtual void Tick(float DeltaTime) override;
+
+	void UpdateColor(float DeltaTime);
+
+	UPROPERTY(EditAnywhere, Category = "Pong|Tint Change")
+	float DefaultColorUpdateSpeed;
+	UPROPERTY(EditAnywhere, Category = "Pong|Tint Change")
+	float StartingPlayColorUpdateSpeed;
+	UPROPERTY(EditAnywhere, Category = "Pong|Tint Change")
+	float HitColorUpdateStep;
+
+	UPROPERTY(EditAnywhere, Category = "Pong|Tint Change")
+	float DefaultColorAngle;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Pong|Tint Change")
+	UCurveLinearColor* ColorCurve;
+
+private:
+
+	float mColorUpdateSpeed;
+	float mColorAngle;
 };
